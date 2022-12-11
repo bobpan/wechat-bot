1dd43428a0327b0d16646e0ce3330db4827a131f
小小的天 <bobpan@users.noreply.github.com>
2022-12-11 13:39:06 +0800

    Update sendMessage.js
    修改昵称

--- src/wechaty/sendMessage.js
+++ src/wechaty/sendMessage.js
@@ -2,7 +2,7 @@ import { getChatGPTReply } from '../chatgpt/index.js'
 
 // 定义机器人的名称，这里是为了防止群聊消息太多，所以只有艾特机器人才会回复，
 // TODO 记得修改成你自己的微信名称 ↓
-const botName = '@荣顶'
+const botName = '@Bob'
 // 群聊白名单，白名单内的群聊才会自动回复
 const roomWhiteList = ['前端超人技术交流群', '这里填写更加多的群聊名称']
 // 联系人白名单，白名单内的联系人才会自动回复
